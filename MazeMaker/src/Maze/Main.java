ecbb93061b787302033a55bff0c43abd6390ba01
Louis <louis_yanagisawa@technologyonecorp.com>
2022-04-27 14:13:18 +1000

    Adding GUI to main

--- MazeMaker/src/Maze/Main.java
+++ MazeMaker/src/Maze/Main.java
@@ -1,7 +1,36 @@
 package Maze;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        //...TODO
+        JFrame jframe = new JFrame("Maze");
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        jframe.setPreferredSize(new Dimension(400,400));
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add("1");
+        fileMenu.add("2");
+        fileMenu.add("3");
+        menuBar.add(fileMenu);
+        JMenu editMenu = new JMenu("Edit");
+        editMenu.add("1");
+        editMenu.add("2");
+        editMenu.add("3");
+        menuBar.add(editMenu);
+        jframe.setJMenuBar(menuBar);
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        label.setText("Maze");
+        panel.add(label);
+        jframe.add(panel);
+
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
     }
 }
