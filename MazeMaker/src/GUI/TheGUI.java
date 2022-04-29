2de4648d7042a214a5ba7435c6447a7fb13e28e1
Philllipe <phillipe.sebastiao@hotmail.com>
2022-04-29 19:31:14 +1000

    updated javadoc

--- MazeMaker/src/GUI/TheGUI.java
+++ MazeMaker/src/GUI/TheGUI.java
@@ -2,13 +2,14 @@ package GUI;
 
 import javax.swing.*;
 
-public class TheGUI {
-    private JPanel window;
+public class TheGUI extends JFrame {
+    public JPanel window;
     private JPanel mazeAreaPanel;
     private JPanel optionsPanel;
     private JButton solveMazeButton;
     private JButton changeLogoButton;
     private JButton changeStartImageButton;
     private JButton changeEndImageButton;
-
 }
+
+
--- MazeMaker/src/Maze/Main.java
+++ MazeMaker/src/Maze/Main.java
@@ -1,36 +1,19 @@
 package Maze;
 
+import GUI.TheGUI;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Locale;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        JFrame jframe = new JFrame("Maze");
-        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        jframe.setPreferredSize(new Dimension(400,400));
-
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        fileMenu.add("1");
-        fileMenu.add("2");
-        fileMenu.add("3");
-        menuBar.add(fileMenu);
-        JMenu editMenu = new JMenu("Edit");
-        editMenu.add("1");
-        editMenu.add("2");
-        editMenu.add("3");
-        menuBar.add(editMenu);
-        jframe.setJMenuBar(menuBar);
-
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel();
-        label.setText("Maze");
-        panel.add(label);
-        jframe.add(panel);
-
-        jframe.pack();
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
+        JFrame frame = new JFrame("app");
+        frame.setContentPane(new TheGUI().window);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
