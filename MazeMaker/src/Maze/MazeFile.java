5b88be1188e4f936db3d394c74be87064b63cc2e
Ethan <ethiom1001@gmail.com>
2022-06-05 20:36:20 +1000

    Added fundamentals of Databases

--- MazeMaker/src/Maze/MazeFile.java
+++ MazeMaker/src/Maze/MazeFile.java
@@ -4,10 +4,16 @@ import GUI.EditMaze;
 
 import javax.swing.*;
 import java.io.*;
+import java.sql.*;
 
 import static Maze.Grid.grid;
 
+
 public class MazeFile {
+
+    public static Connection db = null;
+    private static Statement stmt = null;
+
 
     public static void saveMaze() throws IOException {
         JFileChooser fileChooser = new JFileChooser();
@@ -96,4 +102,83 @@ public class MazeFile {
 
         }
     }
+
+    public static int connectToDB() {
+        // Currently hard coded, link to db.props
+        String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+        String DB_URL = "jdbc:mariadb://localhost:3306";
+
+        //  Database credentials
+        String USER = "user";
+        String PASS = "hdsajkhd";
+
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Open a connection
+            System.out.println("Connecting to a selected database...");
+            db = DriverManager.getConnection(
+                    DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            System.out.println("Checking if table 'Mazes' exists");
+            String sql = "SELECT EXISTS (" +
+                    "SELECT " +
+                    "   TABLE_NAME " +
+                    "FROM " +
+                    "   information_schema.TABLES" +
+                    "WHERE" +
+                    "   TABLE_NAME = 'Mazes');";
+
+            if (!stmt.execute(sql)) {
+                System.out.println("Table 'Mazes' does not exist, initialising");
+                sql = "CREATE TABLE 'Mazes'(" +
+                        "'ID' int NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+                        "'Author' varchar(256) NOT NULL KEY DEFAULT ''," +
+                        "'Maze Name' varchar(256) KEY," +
+                        "'Height' int NOT NULL DEFAULT 0," +
+                        "'Width' int NOT NULL DEFAULT 0," +
+                        "'Maze Data' BLOB)";
+                stmt.executeUpdate(sql);
+            }
+            System.out.println("Table 'Mazes' exists");
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+            return 0;
+        } catch (LinkageError | ClassNotFoundException e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
+
+    public static void disconnectFromDB() {
+        try {
+            if (stmt != null) {
+                db.close();
+            }
+        } catch (SQLException se) {
+        }// do nothing
+        try {
+            if (db != null) {
+                db.close();
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        System.out.println("Disconnected from database");
+    }
+
+    public static void queryDB(String query) {
+        try {
+            stmt = db.createStatement();
+        } catch (SQLException e) {
+            // TODO - Implement exception handling
+        }
+    }
+
 }
