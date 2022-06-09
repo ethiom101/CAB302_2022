57f0c0f493048091264bc97ec7a66591e6bdcaa7
Philllipe <phillipe.sebastiao@hotmail.com>
2022-06-10 02:20:44 +1000

    GUI for browsing mazes

--- MazeMaker/src/GUI/BrowseMaze.java
+++ MazeMaker/src/GUI/BrowseMaze.java
@@ -1,55 +1,102 @@
 package GUI;
 
+import Maze.MazeSolver;
+
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * GUI for the browse maze page
- */
 public class BrowseMaze extends JFrame {
+    JScrollPane browseArea = new JScrollPane();
+    JPanel Mazes = new JPanel();
+    // distance between each maze area
+    private int height = 225;
+    private int count = 0;
 
-    public BrowseMaze(){
-        initUI();
+    public BrowseMaze() {
+        init();
+        pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
     }
 
-    public void initUI(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
-        panel.setLayout(new GridLayout(1,2));
+    // Initialise frame
+    public void init() {
+        this.setTitle("Maze Maker");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(new BorderLayout());
 
-        JButton SearchButton = new JButton("Search");
-        JButton OpenButton = new JButton("Open");
-        JLabel name = new JLabel("Name");
-        JLabel author = new JLabel("Author");
-        JTextField nameInput = new JTextField();
-        JTextField authorInput = new JTextField();
+        browseArea.setPreferredSize(new Dimension(800, 700));
+        browseArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        browseArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        browseArea.setViewportView(Mazes);
+        Mazes.setBackground(new Color(234, 234, 234));
+        this.add(browseArea);
+    }
 
-        JList MazeList = new JList();
-        MazeList.setFixedCellWidth(200);
-        //Need to give scroll pane data
-        JScrollPane scroller = new JScrollPane();
-        scroller
-                .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroller
-                .setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scroller.setMinimumSize(new Dimension(200, 150));
-        scroller.setPreferredSize(new Dimension(250, 150));
-        scroller.setMaximumSize(new Dimension(250, 200));
+    // details for a maze
+    public void addMaze(String title, String author, String dateCreated, String lastEdited) {
+        JPanel maze = new JPanel();
+        maze.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 5));
+        maze.setBorder(BorderFactory.createLineBorder(new Color(181, 181, 181), 2, true));
+        maze.setBackground(Color.white);
+        maze.setPreferredSize(new Dimension(700, 220));
 
-        panel.add(scroller);
-        panel.add(name);
-        panel.add(nameInput);
-        panel.add(author);
-        panel.add(authorInput);
-        panel.add(SearchButton);
-        panel.add(OpenButton);
+        JLabel mazeDetails = new JLabel();
+        mazeDetails.setPreferredSize(new Dimension(210, 160));
+        mazeDetails.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
 
+        // details section
+        JLabel mazeTitle = new JLabel("Title: " + title);
+        mazeTitle.setPreferredSize(new Dimension(1000, 35));
 
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Browse Maze");
-        frame.pack();
-        frame.setVisible(true);
+        JLabel mazeAuthor = new JLabel("Author: " + author);
+        mazeAuthor.setPreferredSize(new Dimension(1000, 35));
+
+        JLabel mazeDateCreated = new JLabel("Date Created: " + dateCreated);
+        mazeDateCreated.setPreferredSize(new Dimension(1000, 35));
+
+        JLabel mazeLastEdited = new JLabel("Last Edited: " + lastEdited);
+        mazeLastEdited.setPreferredSize(new Dimension(1000, 35));
+
+        mazeDetails.add(mazeTitle);
+        mazeDetails.add(mazeAuthor);
+        mazeDetails.add(mazeDateCreated);
+        mazeDetails.add(mazeLastEdited);
+
+        // maze image section
+        JLabel mazeImage = new JLabel();
+        mazeImage.setPreferredSize(new Dimension(190, 190));
+        mazeImage.setBorder(BorderFactory.createLineBorder(new Color(181, 181, 181), 2, true));
+
+        // buttons
+        JLabel buttons = new JLabel();
+        buttons.setBackground(Color.red);
+        buttons.setPreferredSize(new Dimension(200, 200));
+        buttons.setLayout(new FlowLayout(FlowLayout.LEFT, 100, 20));
+
+        JButton open = new JButton("Open");
+        open.setPreferredSize(new Dimension(100, 40));
+
+        JButton edit = new JButton("Edit");
+        edit.setPreferredSize(new Dimension(100, 40));
+
+        JButton export = new JButton("Export");
+        export.setPreferredSize(new Dimension(100, 40));
+
+        buttons.add(open);
+        buttons.add(edit);
+        buttons.add(export);
+
+        // adding components
+        maze.add(mazeDetails);
+        maze.add(mazeImage);
+        maze.add(buttons);
+
+        // adding to scroll panel screen
+        count++;
+        Mazes.setPreferredSize(new Dimension(800, (height * count) + 20)); // 20 is an offset to allow space at bottom of frame
+        Mazes.add(maze);
     }
 }
